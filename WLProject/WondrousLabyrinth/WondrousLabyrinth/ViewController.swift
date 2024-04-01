549cb3aa80dadeceabe9cc6e66257d04edb82fdc
Mim0sa <mim0sa@qq.com>
2024-04-01 20:25:46 +0800

    create pj

---
+++ WLProject/WondrousLabyrinth/WondrousLabyrinth/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WondrousLabyrinth
+//
+//  Created by 吉彦臻 on 2024/4/1.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
